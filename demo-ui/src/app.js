2a9480d3d773d0bdc0b8a8b0d581293d40231c88
Eric Wiese <eric@ericwie.se>
2023-11-07 14:38:54 -0600

    Add common widgets to demo UI

--- demo-ui/src/app.js
+++ demo-ui/src/app.js
@@ -26,6 +26,8 @@ search.addWidgets([
       item: (hit, { html, components }) => html`
 <article>
   <h1>${components.Highlight({hit, attribute: "name"})}</h1>
+  <p>${components.Highlight({ hit, attribute: 'description' })}</p>
+  <p>$ ${hit.price}</p>
 </article>
 `,
     },
@@ -35,6 +37,65 @@ search.addWidgets([
   }),
   instantsearch.widgets.pagination({
     container: '#pagination',
+  }),
+
+  
+]);
+
+
+search.addWidgets([
+  instantsearch.widgets.sortBy({
+    container: '#sort-by',
+    items: [
+      { label: 'Featured', value: 'algolia-assignment' },
+      { label: 'Popularity', value: 'algolia-assignment_popularity_desc' },
+      { label: 'Price (Low to High)', value: 'algolia-assignment_price_asc' },
+      { label: 'Price (High to Low)', value: 'algolia-assignment_price_desc' },
+    ],
+  }),
+
+  instantsearch.widgets.clearRefinements({
+    container: '#clear-refinements',
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#brand-list',
+    attribute: 'brand',
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#categories-list',
+    attribute: 'categories',
+  }),
+
+  instantsearch.widgets.toggleRefinement({
+    container: '#free-shipping-list',
+    attribute: 'free_shipping',
+    templates: {
+      labelText({ count }, { html }) {
+        return html`Free shipping (${count.toLocaleString()})`;
+      },
+    },
+  }),
+
+  instantsearch.widgets.numericMenu({
+    container: '#price-list',
+    attribute: 'price',
+    items: [
+      { label: 'All' },
+      { label: 'Less than $500', end: 500 },
+      { label: 'Between $500 - $1000', start: 500, end: 1000 },
+      { label: 'More than $1000', start: 1000 },
+    ],
+  }),
+
+  instantsearch.widgets.ratingMenu({
+    container: '#rating-list',
+    attribute: 'rating',
+  }),
+
+  instantsearch.widgets.configure({
+    hitsPerPage: 8
   }),
 ]);
 
